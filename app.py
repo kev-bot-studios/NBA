f38dea9d2e586a4bc7bba7ba60206027ec65b075
Kevin Cory <kcoryrr123@gmail.com>
2020-09-08 00:43:03 -0400

    app.server is needed

--- app.py
+++ app.py
@@ -250,6 +250,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
 
+server = app.server
+
 colors = {
     'background': "#082255",
     'text': "#fff", 
